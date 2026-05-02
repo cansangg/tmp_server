cd21d8392c9a3dbef012a915d6258474bb7df1ee
ltm <1802479661@qq.com>
2026-05-03 01:14:48 +0800

    666

--- b.cpp
+++ b.cpp
@@ -101,7 +101,7 @@ int main() {
 
     auto onClientData = [&](my::TcpSocket& client) -> void {
         std::string c = client.readExactly(1);
-        std::cout << 
+        std::cout << "#" << c << std::endl;
         if (!gameover) {
             if (c == "U") try_rotate(), std::cout << client.getFd() << " pressed U" << std::endl;
             if (c == "L") try_move(0, -1), std::cout << client.getFd() << " pressed L" << std::endl;

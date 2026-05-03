81ae68bc307d13b3b624f8e6d81a0de1bd82ab77
ltm <1802479661@qq.com>
2026-05-03 20:32:52 +0800

    666

--- b.cpp
+++ b.cpp
@@ -102,7 +102,7 @@ int main() {
 
     auto onClientData = [&](my::TcpSocket& client) -> void {
         std::string c = client.readExactly(1);
-        if (client.getFd() > 5 && c.size()) {
+        if (client.getFd() == 4 && c.size()) {
             int old_block = current_block;
             current_block = rnd() % blocks.size();
             if (!try_move(0, 0)) current_block = old_block;
